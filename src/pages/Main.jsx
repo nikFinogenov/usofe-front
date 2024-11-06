be1b29466b8af7fa194ba3ca1b606cece69ab4f0
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-11-06 12:00:26 +0200

    main page updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -46,8 +46,8 @@ function Main() {
             )}
 
             {/* Grid of Remaining Posts */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 max-w-5xl w-full mb-5">
-                {posts.slice(2).map((post) => (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4 max-w-5xl w-full mb-10">
+                {posts.slice(1, 7).map((post) => (
                     <Link key={post.id} to={`/post/${post.id}`} className="hover:shadow-xl transition-shadow duration-300">
                         <Post post={post} />
                     </Link>
