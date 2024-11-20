ffcee8265507e5c096ad663a3a4dfb40f4cae39d
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-11-20 16:57:58 +0200

    email confirmed check

--- src/services/index.js
+++ src/services/index.js
@@ -2,22 +2,36 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { NotifyContext } from "../context/NotifyContext";
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from "../context/AuthContext";
 
 const API_URL = process.env.REACT_APP_API;
 
 const api = axios.create({
     withCredentials: true,
-    baseURL: API_URL
+    baseURL: API_URL,
 });
 
 const AxiosInterceptor = () => {
     const navigate = useNavigate();
     const showNotification = useContext(NotifyContext);
-    const { logout } = useContext(AuthContext);
+    const { logout, user } = useContext(AuthContext); // Получаем пользователя из AuthContext
 
     api.interceptors.request.use((config) => {
-        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+
+        if (user && !user.emailConfirmed) {
+
+            const controller = new AbortController();
+            config.signal = controller.signal; // Привязываем сигнал отмены к запросу
+
+            controller.abort();
+            showNotification("Please confirm your email to perform this action.", "error");
+
+            // Возвращаем запрос с отмененным сигналом
+            return config; // Запрос все равно не выполнится из-за аборта
+        }
+
+        // Добавляем токен авторизации
+        config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
         return config;
     });
 
@@ -26,20 +40,23 @@ const AxiosInterceptor = () => {
             return config;
         },
         async (error) => {
-            if (error.response.status === 401) {
-                // localStorage.removeItem('token');
-                logout();
-                navigate('/login');
-                showNotification('Session expired. Please log in again.', 'error');
+            if (axios.isCancel(error)) {
+                console.log("Request was canceled:", error.message);
+                return; // Ничего не делаем, запрос уже отменен
             }
-            if(error.response.status === 403) {
-
+            if (error.response.status === 401) {
+                logout();
+                navigate("/login");
+                showNotification("Session expired. Please log in again.", "error");
+            }
+            if (error.response.status === 403) {
+                showNotification("Access denied. You do not have permission to perform this action.", "error");
             }
             throw error;
         }
     );
 
-    return null; // этот компонент ничего не рендерит, его задача — настроить интерцепторы
+    return null; // Этот компонент ничего не рендерит, его задача — настроить интерцепторы
 };
 
 export { api, AxiosInterceptor };
