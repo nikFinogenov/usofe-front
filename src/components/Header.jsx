0ffee2735afc2efff304cb79adb1cdd4222b33d7
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-12-01 02:51:25 +0200

    header searchbar fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,6 @@ function Header() {
             }
 
             navigate(`/search?q=${encodeURIComponent(searchText.trim())}`);
-            setSearchText('');
             setShowSuggestions(false);
         } catch (error) {
             console.error('Error during search:', error);
@@ -41,13 +40,23 @@ function Header() {
     const handleSearchButtonMouseDown = (e) => {
         e.preventDefault();  // Prevent the blur event from being triggered
     };
+    
+    const handleLabelClick = (e) => {
+        setSearchText('');
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
 
     return (
         <header className="fixed top-0 left-0 right-0 bg-blue-500 text-white py-3 px-4 z-10 shadow-md">
             <nav className="flex items-center">
                 {/* Заголовок */}
                 <h1 className="text-2xl font-bold">
-                    <Link to="/">My Forum</Link>
+                    <Link to="/" onClick={handleLabelClick}>My Forum</Link>
                 </h1>
 
                 {/* Поисковая строка */}
@@ -59,6 +68,7 @@ function Header() {
                         onChange={handleInputChange}
                         onBlur={handleBlur}  // Close suggestions on blur
                         onFocus={handleFocus} // Show suggestions on focus
+                        onKeyDown={handleKeyDown} // Trigger search on Enter
                         className="w-full px-4 py-2 rounded text-black focus:outline-none focus:ring-2 focus:ring-blue-300"
                     />
                     {showSuggestions && (
